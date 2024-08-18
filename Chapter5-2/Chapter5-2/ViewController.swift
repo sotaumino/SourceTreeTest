2dfc8409f5e46a8de1cdba2b9a3dc63ba051e765
umino <sota.umino@axas-japan.co.jp>
2024-08-18 20:53:52 +0900

    追加忘れ

--- Chapter5-2/Chapter5-2/ViewController.swift
+++ Chapter5-2/Chapter5-2/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // UserDefaultsの参照
+        let userDefaults = UserDefaults.standard
+        
+        // UserDefaultsからtextキーに対応する値を参照
+        if let value = userDefaults.string(forKey: "text"){
+            // 取り出した値をテキストフィールドに設定
+            textField.text = value
+        }
     }
 
-
+    @IBAction func tapActionButton(_ sender: Any) {
+        // userDefaultsの参照
+        let userDefaults = UserDefaults.standard
+        
+        // UserDefaultsにtextFieldの値を保存する
+        userDefaults.set(textField.text, forKey: "text")
+        
+        // UserDefaultsの値を同期処理
+        userDefaults.synchronize()
+    }
 }
 
